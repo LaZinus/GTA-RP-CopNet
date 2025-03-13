e3f3966c965293617d08d000c0ac66b3515e2fd7
Damon.Buchholz <damon.buchholz@horn-goerwitz.de>
2025-03-13 12:09:31 +0100

    fixed dragged elements and added hover effekt

--- html/script.js
+++ html/script.js
@@ -13,6 +13,13 @@ function allowDrop(event) {
 
 function drag(event) {
     event.dataTransfer.setData("text", event.target.closest("td").dataset.index);
+
+    let emptySlots = document.querySelectorAll(".empty-slot");
+
+    emptySlots.forEach(slot => {
+        slot.style.background = "rgba(255, 255, 255, 0.05)"
+        slot.style.border = "2px dashed rgba(255, 255, 255, 0.2)"
+    })
 }
 
 function drop(event) {
@@ -34,6 +41,9 @@ function drop(event) {
         if (targetElement.classList.contains("empty-slot")) {
             targetElement.classList.remove("empty-slot");
             targetElement.classList.add("app-slot");
+            targetElement.style.border = "none"
+            targetElement.addEventListener("mouseover", mouseOver);
+            targetElement.addEventListener("mouseout", mouseOut);
             targetElement.setAttribute('draggable', true);
             targetElement.setAttribute('ondragstart', "drag(event)");
             targetElement.setAttribute('ondrop', "drop(event)");
@@ -44,12 +54,21 @@ function drop(event) {
             draggedElement.removeAttribute('ondragstart');
             draggedElement.setAttribute('ondrop', "drop(event)");
             draggedElement.setAttribute('ondragover', "allowDrop(event)");
+            draggedElement.removeEventListener("mouseover", mouseOver);
+            draggedElement.removeEventListener("mouseout", mouseOut);
         }
 
         // Datenindex umtauschen, damit die Logik stimmt
         let tempIndex = targetElement.dataset.index;
         targetElement.dataset.index = draggedElement.dataset.index;
         draggedElement.dataset.index = tempIndex;
+
+        let emptySlots = document.querySelectorAll(".empty-slot");
+
+        emptySlots.forEach(slot => {
+            slot.style.background = "none"
+            slot.style.border = "none"
+        })
     }
 }
 
@@ -58,3 +77,11 @@ document.querySelectorAll("td").forEach(td => {
     td.ondrop = drop;
     td.ondragover = allowDrop;
 });
+
+function mouseOver(event) {
+    event.target.style.background = "rgba(255, 255, 255, 0.2)";
+}
+
+function mouseOut(event) {
+    event.target.style.background = "rgba(255, 255, 255, 0.1)";
+}
